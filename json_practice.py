e143c90927852fde17369ab612f3e98eeb76ecda
Wasiq Wani <wasiqmajeedwani123@outlook.com>
2024-03-15 22:24:53 +0100

    Added a new phone to the list of phones in practice_file.json

--- json_practice.py
+++ json_practice.py
@@ -1,10 +1,17 @@
 import json
 
+
 with open("practice_file.json", "r") as f:
     data = json.load(f)
-    phones = data['electronic_store']['departments']['smartphones']
+    data['electronic_store']['departments']['smartphones']['brands'].append('Nokia')
+    print(data)
 
+    # print(phones)
+    # with open('phones_for_sale.json', 'w') as f1: # Add the phones for sale to a new JSON file
+    #     json.dump(phones, f1, indent=1)
 
-    with open('phones_for_sale.json', 'w') as f1:
-        json.dump(phones, f1, indent=1)
+# Add new phone to the list of phones in "practice_file.json"
+with open('practice_file.json', 'w') as f:
+    json.dump(data, f, indent=2)
+
 
