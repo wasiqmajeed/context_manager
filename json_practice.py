8c67a061c67e7970dbf4d893f5efe340e82299b2
Wasiq Wani <wasiqmajeedwani123@outlook.com>
2024-03-15 23:53:36 +0100

    Removed the Asus laptop at the 4th Index

--- json_practice.py
+++ json_practice.py
@@ -1,16 +1,16 @@
 import json
 
-
 with open("practice_file.json", "r") as f:
     data = json.load(f)
-    data['electronic_store']['departments']['smartphones']['brands'].append('Nokia')
-    print(data)
+    # data['electronic_store']['departments']['smartphones']['brands'].append('Nokia') # Append a new phone to data
+    # del data['electronic_store']['departments']['laptops']['brands'][4]  # Delete the phone at the 4th index of the
+    # list
 
     # print(phones)
     # with open('phones_for_sale.json', 'w') as f1: # Add the phones for sale to a new JSON file
     #     json.dump(phones, f1, indent=1)
 
-# Add new phone to the list of phones in "practice_file.json"
+# Opening the file in write mode and adding changes
 with open('practice_file.json', 'w') as f:
     json.dump(data, f, indent=2)
 
