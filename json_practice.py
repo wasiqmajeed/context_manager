06387c5432f615b6353d96e4212c99ebe4878a2c
Wasiq Wani <wasiqmajeedwani123@outlook.com>
2024-03-16 22:53:57 +0100

    Making the code a bit cleaner

--- json_practice.py
+++ json_practice.py
@@ -2,13 +2,15 @@ import json
 
 with open("practice_file.json", "r") as f:
     data = json.load(f)
-    # data['electronic_store']['departments']['smartphones']['brands'].append('Nokia') # Append a new phone to data
-    # del data['electronic_store']['departments']['laptops']['brands'][4]  # Delete the phone at the 4th index of the
-    # list
+    # Append a new phone to data
+    data['electronic_store']['departments']['smartphones']['brands'].append('Nokia')
 
-    # print(phones)
-    # with open('phones_for_sale.json', 'w') as f1: # Add the phones for sale to a new JSON file
-    #     json.dump(phones, f1, indent=1)
+    # Delete the laptop at the 4th index of the list
+    del data['electronic_store']['departments']['laptops']['brands'][4]
+
+    phones = data['electronic_store']['departments']['smartphones']['brands']
+    with open('phones_for_sale.json', 'w') as f1:  # Add the phones for sale to a new JSON file
+        json.dump(phones, f1, indent=1)
 
 # Opening the file in write mode and adding changes
 with open('practice_file.json', 'w') as f:
--- yaml_practice.py
+++ yaml_practice.py
@@ -8,9 +8,15 @@ new_review = {'comment': 'This shop has amazing products and great deals',
 
 with open('practice_file.yaml', 'r+') as file:
     data = yaml.safe_load(file)
-    # data["electronic_store"]['customer_reviews'].append(new_review) # Adding a new review for the shop in yaml file
-    # data["electronic_store"]['departments']['laptops']['stock'][1]['price'] = 549.99  # Updating the price of Lenovo
-    # laptop to 549.99
+
+    # Adding a new review for the shop in yaml
+    data["electronic_store"]['customer_reviews'].append(new_review)
+
+    # Updating the price of Lenovo laptop to 549.99
+    data["electronic_store"]['departments']['laptops']['stock'][1]['price'] = 549.99
+
+    #  Adding the address of the shop
+    data['electronic_store']['address'] = 'Kazimierza Brokla 2, 00-032 Warszawa'
 
 
 with open('practice_file.yaml', 'w') as file:
